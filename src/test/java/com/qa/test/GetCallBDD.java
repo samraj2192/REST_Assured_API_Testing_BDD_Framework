e53b1384e498ce8ebbd88d5d9d73300583d98fc9
samraj2192 <samraj.bhowmik@gmail.com>
2020-02-21 23:04:32 +0530

    added @beforeTest

--- src/test/java/com/qa/test/GetCallBDD.java
+++ src/test/java/com/qa/test/GetCallBDD.java
@@ -1,5 +1,13 @@
 package com.qa.test;
 
+import org.testng.annotations.BeforeTest;
+
 public class GetCallBDD {
+	
+	//new code will be added
+	@BeforeTest
+	public static void initialize() {
+		
+	}
 
 }
